511139f3bd2758a13e89dde82ee2c62fc4dc79ef
Jasper Kennis <jasper@fontanel.nl>
2013-04-24 20:53:51 +0200

    Made a demo show of the data.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,22 +6,26 @@ class PagesController < ApplicationController
     if cookies[:luser].nil?
       request_token = @client.request_token(oauth_callback: 'http://0.0.0.0:3000')
       rtoken = request_token.token
-      rsecret = request_token.secret
       
       if params[:oauth_verifier].nil?
-        @oauth_url = request_token.authorize_url # Add scope
+        session[:rsecret] = request_token.secret
+        @oauth_url = request_token.authorize_url
         @page = 'beforelinkedin'
       else
         token = @client.authorize_from_request( params[:oauth_token], session[:rsecret], params[:oauth_verifier] )
-        cookies[:luser] =  Marshal.dump( token )
+        cookies[:luser] = Marshal.dump( token )
         redirect_to '/'
       end
     else
       @linkedinvalid = true
       token = Marshal.load( cookies[:luser] )
       @client.authorize_from_access( token[0], token[1])
-      @profile = @client.profile
-#       @connections = @client.connections
+      unless session[:connections]
+        @connections = @client.connections
+        session[:connections] = Marshal.dump( @connections )
+      else
+        @connections = Marshal.load( session[:connections] )
+      end
     end
   end
   
