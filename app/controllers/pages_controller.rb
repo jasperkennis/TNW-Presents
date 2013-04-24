06c089292286eda9a1f60baacfa8f0abe986edc0
Jasper Kennis <jasper@fontanel.nl>
2013-04-24 18:47:24 +0200

    Fixed oneclick signup!

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,35 +1,28 @@
 class PagesController < ApplicationController
   def home
-#     unless params[:code].nil? or params[:code].empty?
-#       require 'net/http'
-#       url = URI.parse( "https://www.linkedin.com/uas/oauth2/accessToken?grant_type=authorization_code&code=#{ params[:code] }&redirect_uri=#{ Settings.redirect_url }&client_id=#{ Settings.linkedin_key }&client_secret=#{ Settings.linkedin_secret }" )
-#       resp, data = Net::HTTP.get( url )
-#       raise data.inspect
-#     end
-    if session[:luser].nil?
-      create_client
-      @rtoken = @client.request_token.token
-      @rsecret = @client.request_token.secret
+    @linkedinvalid = false
+    create_client
+    
+    if cookies[:luser].nil?
+      request_token = @client.request_token(oauth_callback: 'http://0.0.0.0:3000')
+      rtoken = request_token.token
+      rsecret = request_token.secret
       
-      # to test from your desktop, open the following url in your browser
-      # and record the pin it gives you
-      if params[:pin].nil?
-  #     raise @client.inspect
-        @oauth_url = @client.request_token.authorize_url
+      if params[:oauth_verifier].nil?
+        @oauth_url = request_token.authorize_url # Add scope
+        @page = 'beforelinkedin'
       else
-        token = @client.authorize_from_request( params[:pintoken], params[:pinsecret], params[:pin] )
-        session[:luser] = token.serialize
+        token = @client.authorize_from_request( params[:oauth_token], session[:rsecret], params[:oauth_verifier] )
+        cookies[:luser] =  Marshal.dump( token )
+        redirect_to '/'
       end
+    else
+      @linkedinvalid = true
+      token = Marshal.load( cookies[:luser] )
+      @client.authorize_from_access( token[0], token[1])
+      @profile = @client.profile
+#       @connections = @client.connections
     end
-    
-    # then fetch your access keys
-#     client.authorize_from_request(@rtoken, @rsecret, pin)
-#     => ["OU812", "8675309"] # <= save these for future requests
-    
-    # or authorize from previously fetched access keys
-#     client.authorize_from_access("OU812", "8675309")
-    
-    # you're now free to move about the cabin, call any API method
   end
   
   def authorize
