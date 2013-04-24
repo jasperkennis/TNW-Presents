fc63b119d799dfc5d1434f1707d651ceebb5974a
Jasper Kennis <jasper@fontanel.nl>
2013-04-24 15:17:48 +0200

    There, i fixed it.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TnwPresents::Application.config.secret_token = '602f3b3f0dfd6ba5c2a2786cf63e31627f6736183daa65721270d915e4bf4f8a1351020306280c5825581da83af0920f5250d5c9779a7577c8f41b4098106b20'
