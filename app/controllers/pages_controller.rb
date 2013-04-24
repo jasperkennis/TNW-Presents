f77d9f5bbc6fb4055c3c065bdb9bbfac00b32c99
Jasper Kennis <jasper@fontanel.nl>
2013-04-24 16:37:42 +0200

    Creating a login url.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,16 @@
 class PagesController < ApplicationController
   def home
-    client = LinkedIn::Client.new( Settings.linkedin_key, Settings.linkedin_secret )
-    rtoken = client.request_token.token
-    rsecret = client.request_token.secret
+    create_client
+    rtoken = @client.request_token.token
+    rsecret = @client.request_token.secret
     
     # to test from your desktop, open the following url in your browser
     # and record the pin it gives you
-    client.request_token.authorize_url
+    @oauth_url = @client.request_token.authorize_url
 #     => "https://api.linkedin.com/uas/oauth/authorize?oauth_token=<generated_token>"
     
     # then fetch your access keys
-    client.authorize_from_request(rtoken, rsecret, pin)
+#     client.authorize_from_request(rtoken, rsecret, pin)
 #     => ["OU812", "8675309"] # <= save these for future requests
     
     # or authorize from previously fetched access keys
