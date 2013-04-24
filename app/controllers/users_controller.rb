16365f5cb7638c84a14e78697a9ff5813b33c7a6
Jasper Kennis <jasper@fontanel.nl>
2013-04-24 21:37:46 +0200

    Created the logics to fetch a single users details.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
   def show
+    @user_id = params[:id]
+    
+    create_client
+    
+    if cookies[:luser].nil?
+      redirect_to '/'
+    else
+      token = Marshal.load( cookies[:luser] )
+      @client.authorize_from_access( token[0], token[1])
+      unless session["user-#{ @user_id.parameterize }"]
+        @user = @client.profile( id: @user_id, fields: [ :summary, :headline, :first_name, :last_name, :picture_url ] )
+        session["user-#{ @user_id.parameterize }"] = Marshal.dump( @user )
+      else
+        @user = Marshal.load( session["user-#{ @user_id.parameterize }"] )
+      end
+    end
   end
+  
+  def authorize
+    
+  end
+  
+  private
+    def create_client
+      @client ||= LinkedIn::Client.new( Settings.linkedin_key, Settings.linkedin_secret )
+    end
 end
