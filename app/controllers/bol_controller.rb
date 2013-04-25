c421f66236546d4ddd1fc5f8534982f5377f6c6f
Jasper Kennis <jasper@fontanel.nl>
2013-04-25 08:01:10 +0200

    Made the bol suggestions controller respond to json.

--- app/controllers/bol_controller.rb
+++ app/controllers/bol_controller.rb
@@ -2,6 +2,10 @@ class BolController < ApplicationController
   def suggestions
     @term = params[:term]
     @product = Bol.search(@term).limit(1)
-    @recommendations = Bol.recommendations @product.first.id
+    @recommendations = Bol.recommendations( @product.first.id ).limit(4)
+    
+    respond_to do |format|
+      format.json  { render :json => @recommendations }
+    end
   end
 end
