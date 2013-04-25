5b4f82062f5dbc005934f3360583ad4e312c2c21
Jasper Kennis <jasper@fontanel.nl>
2013-04-25 11:06:25 +0200

    Allowing env vars as fallbacks.

--- config/initializers/bol.rb
+++ config/initializers/bol.rb
@@ -1,7 +1,7 @@
 require 'bol'
 
 Bol.configure do |c|
-  c.access_key = Settings.bol_key
-  c.secret = Settings.bol_secret
+  c.access_key = Settings.bol_key or ENV['bol_key']
+  c.secret = Settings.bol_secret or ENV['bol_secret']
   c.per_page = 10
 end
